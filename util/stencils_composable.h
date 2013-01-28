e566db2f8c521e21455f56d25f837f55cfb37e8d
ugo <uvaretto@cscs.ch>
2013-01-28 15:52:43 +0100

    Working on composition

--- util/stencils_composable.h
+++ util/stencils_composable.h
@@ -5,41 +5,78 @@
 
 #include "stencil.h"
 
-
-
 template < typename RetT,
            int X_MIN_OFF = 0, int Y_MIN_OFF = 0, int Z_MIN_OFF = 0,
            int X_MAX_OFF = 0, int Y_MAX_OFF = 0, int Z_MAX_OFF = 0 >
 struct stencil {
-    typedef return_type = RetT;
-    enum { x_min_off = X_MIN_OFF... };
-    enum { x_extent = X_MAX_OFF - X_MIN_OFF ...};
-};
-//allows to treat leaf accessors(the ones that return data)
-//the same as stencils and by doing so it permits to compose
-//stencils at will
-struct dummy_accessor_type {
+    typedef RetT return_type;
+    enum { x_min_off = X_MIN_OFF,
+           y_min_off = Y_MIN_OFF,
+           z_min_off = Z_MIN_OFF,
+           x_max_off = X_MAX_OFF,
+           y_max_off = Y_MAX_OFF,
+           z_max_off = Z_MAX_OFF};
+    enum { x_extent = X_MAX_OFF - X_MIN_OFF,
+           y_extent = Y_MAX_OFF - Y_MIN_OFF,
+           z_extent = Z_MAX_OFF - Z_MIN_OFF}
 };
 
-template < typename T >
-struct accessor : stencil< T >{
-    typedef stencil< T >::return_type return_type;
-    template < typename U,
-               typename AccessorT = dummy_accessor_type >
+struct accessor {
+    template < typename T >
     __host__ __device__
-    return_type operator()(const U* grid,
-                 const dim3& xyz,
-                 const dim3& grid_size,
-                 const AccessorT& acc = AccessorT()) const {
-        return return_type(grid_3d_read(grid, xyz.x, xyz.y, xyz.z, grid_size));
+    T operator()(const T* grid,
+                 int x, int y, int z,
+                 ptrdiff_t row_offset,
+                 ptrdiff_t slice_offset) const {
+        return grid_read(grid, x, y, z, row_offset, slice_offset);
+    }
+    template < typename T >
+    __host__ __device__
+    T operator()(const T* grid,
+                 int x, int y,
+                 ptrdiff_t row_offset) const {
+        return grid_read(grid, x, y, row_offset);
+    }
+    template < typename T >
+    __host__ __device__
+    T operator()(const T* grid,
+                 int x) const {
+        return grid_read(grid, x);
+    }
+    template < typename T >
+    __host__ __device__
+    T operator()(const T* grid) const {
+        return grid_read(grid);
     }
     template <typename T>
     __host__ __device__
     void operator()(T* grid,
                     const T& v,
                     int x, int y, int z,
-                    const dim3& grid_size) const {
-        grid_3d_write(grid, v, x, y, z, grid_size);
+                    ptrdiff_t row_offset,
+                    ptrdiff_t slice_offset) const {
+        grid_write(grid, v, x, y, z, row_offset, slice_offset);
+    }
+    template <typename T>
+    __host__ __device__
+    void operator()(T* grid,
+                    const T& v,
+                    int x, int y,
+                    ptrdiff_t row_offset) const {
+        grid_write(grid, v, x, y, row_offset);
+    }
+    template <typename T>
+    __host__ __device__
+    void operator()(T* grid,
+                    const T& v,
+                    int x) const {
+        grid_write(grid, v, x);
+    }
+    template <typename T>
+    __host__ __device__
+    void operator()(T* grid,
+                    const T& v) const {
+        grid_write(grid, v);
     }
 };
 #ifdef ENABLE_SURFACE
@@ -47,20 +84,15 @@ template < typename T >
 struct surface_accessor {
     __device__
     T operator()(const T* grid,
-                 const dim3& center,
-                 const dim3& grid_size) const {
-        surf3Dread(&v_, in_surface,
-                   center.x * sizeof(T),
-                   center.y, center.z);
+                 int x, int y, int z) const {
+        surf3Dread(&v_, in_surface, x * sizeof(T), y, z);
         return v_;
     }
-    void operator()(const T* grid,
-                 const T& v,
-                 const dim3& center,
-                 const dim3& grid_size) const {
-        surf3Dwrite(v_, in_surface,
-                   center.x * sizeof(T),
-                   center.y, center.z);
+    __device_
+    void operator()(T* grid,
+                    const T& v,
+                    int x, int y, int z) const {
+        surf3Dwrite(v_, in_surface, x * sizeof(T), y, z);
     }
     mutable T v_;
 };
@@ -68,28 +100,16 @@ struct surface_accessor {
 #ifdef ENABLE_TEXTURE
 template < typename T >
 struct texture_accessor {
-    template < typename AccessorT = dummy_accessor_type>
     __device__
-    T operator()(const T* grid,
-                 const dim3& center,
-                 const dim3& grid_size,
-                 const AccessorT& accessor = AccessorT()) const {
-        return tex3D(in_texture,
-                     center.x,
-                     center.y,
-                     center.z);
+    T operator()(const T* grid,int x, int y, int z) const {
+        return tex3D(in_texture, x, y, z);
     }
 };
 template <>
 struct texture_accessor<double> {
     __device__
-    double operator()(const T* grid,
-                      const dim3& center,
-                      const dim3& grid_size) const {
-        v_ = tex3D(in_texture,
-                   center.x,
-                   center.y,
-                   center.z);
+    double operator()(const T* grid, int x, int y, int z) const {
+        v_ = tex3D(in_texture, x, y, z);
         return __hiloint2double(v.y, v.x);
     }
     mutable int2 v_;
@@ -97,53 +117,185 @@ struct texture_accessor<double> {
 #endif
 
 template <typename S1, typename S2>
-struct composite_stencil {
-    __host__ __device__
+struct composite_stencil : 
+    stencil< typename S1::return_type,
+             S1::x_min_off - S2::x_min_off,
+             S1::y_min_off - S2::y_min_off,
+             S2::z_min_off - S2::z_min_off,
+             S1::x_max_off + S2::x_max_off,
+             S1::y_max_off + S2::y_max_off,
+             S2::z_max_off + S2::z_max_off >  {
+    typedef typename S1::return_type return_type;                
+#ifndef __CUDA_ARCH__    
+    __host__
+#endif
+    __device__
     composite_stencil(const S1& s1 = S1(),
                       const S2& s2 = S2()) : s1_(s1), s2_(s2) {}
-    template < typename T > 
-    __host__ __device__
+    template < typename T, typename AccessorT > 
+#ifndef __CUDA_ARCH__    
+    __host__
+#endif 
+    __device__
     T operator()(const T* grid,
-                 const dim3& center,
-                 const dim3& grid_size,
+                 int x, int y, int z,
+                 ptrdiff_t row_offset,
+                 ptrdiff_t slice_offset,
                  const AccessorT& accessor ) const {
-        return s1_(grid, center, grid_size, s2_);
-    S1 s1_;
-    S2 s2_;     
+        return s1_(grid, x, y, z, row_offset, slice_offset,
+                   s2_(grid, x, y, z, row_offset, slice_offset, accessor));
+    }
+    template < typename T, typename AccessorT > 
+#ifndef __CUDA_ARCH__    
+    __host__
+#endif 
+    __device__
+    T operator()(const T* grid,
+                 int x, int y,
+                 ptrdiff_t row_offset,
+                 const AccessorT& accessor ) const {
+        return s1_(grid, x, y, row_offset,
+                   s2_(grid, x, y, row_offset, accessor));
+    }
+    template < typename T, typename AccessorT > 
+#ifndef __CUDA_ARCH__    
+    __host__
+#endif 
+    __device__
+    T operator()(const T* grid,
+                 int x, 
+                 const AccessorT& accessor ) const {
+        return s1_(grid, x,
+                   s2_(grid, x, accessor));
+    }
+    template < typename T, typename AccessorT > 
+#ifndef __CUDA_ARCH__    
+    __host__
+#endif 
+    __device__
+    T operator()(const T* grid, 
+                 const AccessorT& accessor ) const {
+        return s1_(grid,
+                   s2_(grid, accessor));
+    }
+        template < typename T, typename AccessorT > 
+#ifndef __CUDA_ARCH__    
+    __host__
+#endif 
+    __device__
+    T operator()(const T* grid,
+                 ptrdiff_t row_offset,
+                 ptrdiff_t slice_offset, 
+                 const AccessorT& accessor ) const {
+        return s1_(grid, row_offset, slice_offset,
+                   s2_(grid, row_offset, slice_offset, accessor));
+    }                               
+    mutable S1 s1_;
+    mutable S2 s2_;     
 };
 
-struct laplacian_3d {
-    template < typename T, typename AccessorT > 
+//struct index3d {
+//    int x, y, z;
+//};
+
+// template < typename RetT, typename CombinerT >
+// struct fan_in_stencil {
+
+//     RetT operator()(const T1* g1, const T2* g2... ) {
+//         return combine(g1, x, y, z, row_offset1, slice_offset1,
+//                        g2...., );
+//     }  
+// };
+
+// template < typename CombinerT >
+// struct fan_out_stencil {
+
+//     RetT operator()(const T1* gin, T1* g1, T2* g2... ) {
+//         return combine(g1, row_offset1, slice_offset1,
+//                        g2...., );
+//     }  
+// };
+
+
+//==============================================================================
+template <typename T>
+struct laplacian_3d : stencil<T, -1, -1, -1, 1, 1, 1> {
+    template < typename U, typename AccessorT > 
     __host__ __device__
-    T operator()(const T* grid,
-                 const dim3& center,
-                 const dim3& grid_size,
-                 const AccessorT& gv //get value through accessor ) 
-                ) const {
+    return_type operator()(const U* grid,
+                           int x, int y, int z,
+                           ptrdiff_t row_offset,
+                           ptrdiff_t slice_offset,
+                           const AccessorT& gv //get value through accessor ) 
+                          ) const {
         
-        return T(-6) * gv(grid, center.x, center.y, center.z, grid_size)
-             + gv(grid, center.x + 1, center.y, center.z, grid_size)
-             + gv(grid, center.x - 1, center.y, center.z, grid_size)
-             + gv(grid, center.x, center.y + 1, center.z, grid_size)
-             + gv(grid, center.x, center.y - 1, center.z, grid_size)
-             + gv(grid, center.x, center.y, center.z + 1, grid_size)
-             + gv(grid, center.x, center.y, center.z + 1, grid_size);
+        return T(-6) * gv(grid)
+             + gv(grid, x + 1)
+             + gv(grid, x - 1)
+             + gv(grid, x, y + 1, row_offset)
+             + gv(grid, x, y - 1, row_offset)
+             + gv(grid, x, y, z + 1, row_offset, slice_offset)
+             + gv(grid, x, y, z + 1, row_offset, slice_offset);
+
+    }
+    template < typename U, typename AccessorT > 
+    __host__ __device__
+    return_type operator()(const U* grid,
+                           ptrdiff_t row_offset,
+                           ptrdiff_t slice_offset,
+                           const AccessorT& gv //get value through accessor ) 
+                          ) const {
+        
+        return T(-6) * gv(grid)
+             + gv(grid, +1)
+             + gv(grid, -1)
+             + gv(grid, 0, +1, row_offset)
+             + gv(grid, 0, -1, row_offset)
+             + gv(grid, 0, 0, +1, row_offset, slice_offset)
+             + gv(grid, 0, 0, -1, row_offset, slice_offset);
 
     }
 };
-struct diffusion_3d {
+template <typename T>
+struct madd_3d : stencil<T> {
     template < typename T, typename AccessorT >  
     __host__ __device__
     T operator()(const T* grid,
-                 const AccessorT& gv, //get value through accessor 
-                 const dim3& center,
-                 const dim3& grid_size) const {
-        const T v = gv(grid, center.x, center.y, center.z, grid_size);
+                 int x, int y, int z,
+                 ptrdiff_t row_offset,
+                 ptrdiff_t slice_offset,
+                 const AccessorT& gv ) const {
+        const T v = gv(grid);
         //printf("%d %d %d: %f\n", center.x, center.y, center.z, v);
-        return v + T(0.1) * l3d(grid, center, grid_size); 
+        return v + T(0.1) * gv(grid, x, y, z, row_offset, slice_offset); 
     }
-    laplacian_3d l3d;
+    template < typename T, typename AccessorT >  
+    __host__ __device__
+    T operator()(const T* grid,
+                 ptrdiff_t row_offset,
+                 ptrdiff_t slice_offset,
+                 const AccessorT& gv ) const {
+        const T v = gv(grid);
+        //printf("%d %d %d: %f\n", center.x, center.y, center.z, v);
+        return v + T(0.1) * gv(grid, row_offset, slice_offset); 
+    }
 };
+
+template < typename T >
+struct diffusion_3d : composite_stencil< madd_3d< T >, laplacian_3d< T > > {};
+
+template <typename StencilT1, typename StencilT2 >
+composite_stencil<StencilT1, StencilT2> 
+operator >>=(const StencilT1& s1, const StencilT2&s2 s ) {
+    return composite_stencil< S1, S2 >(s1, s2);
+}
+
+// <typename S1, typename S2, typename S3 >
+// struct composite {
+//     typedef composite_stencil<S1, composite_stencil< S2, S3 > > type;
+// };
+
+
 
 
 template < typename T >
